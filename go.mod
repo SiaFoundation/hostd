f8f5cbc3f2d9681fce7cfcfbe0e9f45af61b5742
Nate Maninger <me@n8m.us>
2023-02-03 16:49:00 -0700

    rhp/v3: add siamux compatibility test

--- go.mod
+++ go.mod
@@ -7,8 +7,8 @@ require (
 	github.com/mattn/go-sqlite3 v1.14.16
 	gitlab.com/NebulousLabs/encoding v0.0.0-20200604091946-456c3dc907fe
 	gitlab.com/NebulousLabs/fastrand v0.0.0-20181126182046-603482d69e40 // indirect
-	gitlab.com/NebulousLabs/log v0.0.0-20210609172545-77f6775350e2 // indirect
-	gitlab.com/NebulousLabs/siamux v0.0.2-0.20220630142132-142a1443a259 // indirect
+	gitlab.com/NebulousLabs/log v0.0.0-20210609172545-77f6775350e2
+	gitlab.com/NebulousLabs/siamux v0.0.2-0.20220630142132-142a1443a259
 	go.sia.tech/core v0.1.5
 	go.sia.tech/mux v1.1.1-0.20230119180453-05591decec67
 	go.sia.tech/renterd v0.0.0-20230203174634-267812a549cd
---
+++ rhp/v3/rpc_test.go
@@ -0,0 +1,94 @@
+package rhp
+
+import (
+	"errors"
+	"net"
+	"testing"
+	"time"
+
+	nlog "gitlab.com/NebulousLabs/log"
+	nmux "gitlab.com/NebulousLabs/siamux"
+	smux "gitlab.com/NebulousLabs/siamux/mux"
+	rhpv3 "go.sia.tech/core/rhp/v3"
+	"go.sia.tech/core/types"
+	smods "go.sia.tech/siad/modules"
+	"lukechampine.com/frand"
+)
+
+func startHostTransport(priv types.PrivateKey) (string, error) {
+	l, err := net.Listen("tcp", "localhost:0")
+	if err != nil {
+		return "", err
+	}
+	go func() {
+		conn, err := l.Accept()
+		if errors.Is(err, net.ErrClosed) {
+			return
+		} else if err != nil {
+			panic(err)
+		}
+
+		t, err := rhpv3.NewHostTransport(conn, priv)
+		if err != nil {
+			panic(err)
+		}
+
+		stream, err := t.AcceptStream()
+		if err != nil {
+			panic(err)
+		} else if err := stream.SetDeadline(time.Now().Add(15 * time.Second)); err != nil {
+			panic(err)
+		}
+
+		id, err := stream.ReadID()
+		if err != nil {
+			panic(err)
+		} else if id != rhpv3.RPCUpdatePriceTableID {
+			panic("unexpected id")
+		}
+
+		resp := rhpv3.RPCUpdatePriceTableResponse{
+			PriceTableJSON: []byte("hello world"),
+		}
+		if err := stream.WriteResponse(&resp); err != nil {
+			panic(err)
+		}
+	}()
+	return l.Addr().String(), nil
+}
+
+func TestTransportReadWriteCompat(t *testing.T) {
+	privKey := types.NewPrivateKeyFromSeed(frand.Bytes(32))
+
+	addr, err := startHostTransport(privKey)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	m, err := nmux.New(":0", ":0", nlog.DiscardLogger, t.TempDir())
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	pubKey := privKey.PublicKey()
+	var muxKey smux.ED25519PublicKey
+	copy(muxKey[:], pubKey[:])
+	stream, err := m.NewStream("host", addr, muxKey)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer stream.Close()
+
+	if err := stream.SetDeadline(time.Now().Add(15 * time.Second)); err != nil {
+		t.Fatal(err)
+	}
+
+	if err := smods.RPCWrite(stream, smods.RPCUpdatePriceTable); err != nil {
+		t.Fatal(err)
+	}
+
+	var resp smods.RPCUpdatePriceTableResponse
+	if err := smods.RPCRead(stream, &resp); err != nil {
+		t.Fatal(err)
+	}
+}
