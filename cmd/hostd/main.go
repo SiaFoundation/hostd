31970f9478b04442d280f1666f02bcc8b714ef46
Nate Maninger <me@n8m.us>
2023-03-09 08:26:49 -0700

    cmd: add docker flag to adjust cmd behavior

--- cmd/hostd/main.go
+++ cmd/hostd/main.go
@@ -30,8 +30,12 @@ var (
 	dir         string
 	bootstrap   bool
 
-	logLevel  string
-	logStdOut bool
+	logLevel string
+
+	// adjusts hostd for better docker integration.
+	// - logs to stdout
+	// - disables stdin prompts
+	docker bool
 )
 
 func check(context string, err error) {
@@ -42,24 +46,29 @@ func check(context string, err error) {
 
 func getAPIPassword() string {
 	apiPassword := os.Getenv(apiPasswordEnvVariable)
-	if apiPassword != "" {
+	if len(apiPassword) != 0 {
 		log.Printf("Using %s environment variable.", apiPasswordEnvVariable)
-	} else {
-		fmt.Print("Enter API password: ")
-		pw, err := term.ReadPassword(int(os.Stdin.Fd()))
-		fmt.Println()
-		if err != nil {
-			log.Fatal(err)
-		}
-		apiPassword = string(pw)
+		return apiPassword
+	} else if docker {
+		log.Fatalf("%s must be set via environment variable when running in docker.", apiPasswordEnvVariable)
 	}
+
+	fmt.Print("Enter API password: ")
+	pw, err := term.ReadPassword(int(os.Stdin.Fd()))
+	fmt.Println()
+	if err != nil {
+		log.Fatal(err)
+	}
+	apiPassword = string(pw)
 	return apiPassword
 }
 
 func getWalletKey() types.PrivateKey {
 	phrase := os.Getenv(walletSeedEnvVariable)
-	if phrase != "" {
-		log.Printf("Using %s environment variable", walletSeedEnvVariable)
+	if len(phrase) != 0 {
+		log.Printf("Using %s environment variable.", walletSeedEnvVariable)
+	} else if docker {
+		log.Fatalf("%s must be set via environment variable when running in docker.", walletSeedEnvVariable)
 	} else {
 		fmt.Print("Enter wallet seed: ")
 		pw, err := term.ReadPassword(int(os.Stdin.Fd()))
@@ -75,14 +84,14 @@ func getWalletKey() types.PrivateKey {
 }
 
 func main() {
-	flag.StringVar(&gatewayAddr, "addr", defaultGatewayAddr, "address to listen on for peer connections")
+	flag.StringVar(&gatewayAddr, "rpc", defaultGatewayAddr, "address to listen on for peer connections")
 	flag.StringVar(&rhp2Addr, "rhp2", defaultRHPv2Addr, "address to listen on for RHP2 connections")
 	flag.StringVar(&rhp3Addr, "rhp3", defaultRHPv3Addr, "address to listen on for RHP3 connections")
 	flag.StringVar(&apiAddr, "http", defaultAPIAddr, "address to serve API on")
 	flag.StringVar(&dir, "dir", ".", "directory to store hostd metadata")
 	flag.BoolVar(&bootstrap, "bootstrap", true, "bootstrap the gateway and consensus modules")
-	flag.BoolVar(&logStdOut, "log.stdout", false, "log to stdout instead of file")
 	flag.StringVar(&logLevel, "log.level", "warn", "log level (debug, info, warn, error)")
+	flag.BoolVar(&docker, "docker", false, "setting docker to true adjusts hostd for better docker integration (default false)")
 	flag.Parse()
 
 	log.Println("hostd", build.Version())
@@ -105,10 +114,9 @@ func main() {
 	}
 
 	cfg := zap.NewProductionConfig()
-	if logStdOut {
-		cfg.OutputPaths = []string{"stdout"}
-	} else {
-		cfg.OutputPaths = []string{filepath.Join(dir, "hostd.log")}
+	cfg.OutputPaths = []string{filepath.Join(dir, "hostd.log")}
+	if docker {
+		cfg.OutputPaths = append(cfg.OutputPaths, "stdout")
 	}
 	switch logLevel {
 	case "debug":
@@ -118,7 +126,7 @@ func main() {
 	case "warn":
 		cfg.Level = zap.NewAtomicLevelAt(zap.WarnLevel)
 	default:
-		cfg.Level = zap.NewAtomicLevelAt(zap.ErrorLevel)
+		cfg.Level = zap.NewAtomicLevelAt(zap.WarnLevel)
 	}
 	logger, err := cfg.Build()
 	if err != nil {
@@ -129,22 +137,17 @@ func main() {
 	apiPassword := getAPIPassword()
 	walletKey := getWalletKey()
 
+	apiListener, err := net.Listen("tcp", apiAddr)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer apiListener.Close()
+
 	node, err := newNode(gatewayAddr, rhp2Addr, rhp3Addr, dir, bootstrap, walletKey, logger)
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer node.Close()
-
-	log.Println("p2p listening on:", node.g.Address())
-	log.Println("rhp2 listening on:", node.rhp2.LocalAddr())
-	log.Println("rhp3 listening on:", node.rhp3.LocalAddr())
-	log.Println("host public key:", walletKey.PublicKey())
-
-	l, err := net.Listen("tcp", apiAddr)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer l.Close()
 
 	auth := jape.BasicAuth(apiPassword)
 	web := http.Server{
@@ -155,9 +158,23 @@ func main() {
 		ReadTimeout: 30 * time.Second,
 	}
 
+	if docker {
+		logger.Info("hostd listening on", zap.String("api", apiListener.Addr().String()), zap.String("p2p", string(node.g.Address())), zap.String("rhp2", node.rhp2.LocalAddr()), zap.String("rhp3", node.rhp3.LocalAddr()))
+	} else {
+		log.Println("api listening on:", apiListener.Addr().String())
+		log.Println("p2p listening on:", node.g.Address())
+		log.Println("rhp2 listening on:", node.rhp2.LocalAddr())
+		log.Println("rhp3 listening on:", node.rhp3.LocalAddr())
+		log.Println("host public key:", walletKey.PublicKey())
+	}
+
 	go func() {
-		err := web.Serve(l)
+		err := web.Serve(apiListener)
 		if err != nil && !errors.Is(err, http.ErrServerClosed) {
+			if docker {
+				logger.Error("failed to serve web", zap.Error(err))
+				return
+			}
 			log.Println("ERROR: failed to serve web:", err)
 		}
 	}()
@@ -166,7 +183,11 @@ func main() {
 	signalCh := make(chan os.Signal, 1)
 	signal.Notify(signalCh, os.Interrupt, syscall.SIGTERM)
 	<-signalCh
-	log.Println("Shutting down...")
+	if docker {
+		logger.Info("shutdown initiated")
+	} else {
+		log.Println("Shutting down...")
+	}
 	time.AfterFunc(5*time.Minute, func() {
 		os.Exit(-1)
 	})
