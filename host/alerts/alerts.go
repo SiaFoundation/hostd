4e187c22f2264790959ac94f0efbcd658d3f2ce7
Nate Maninger <me@n8m.us>
2023-05-31 12:03:49 -0600

    alerts: add alert manager

---
+++ host/alerts/alerts.go
@@ -0,0 +1,127 @@
+package alerts
+
+import (
+	"fmt"
+	"strings"
+	"sync"
+
+	"go.sia.tech/core/types"
+)
+
+const (
+	// SeverityInfo indicates that the alert is informational.
+	SeverityInfo Severity = iota + 1
+	// SeverityWarning indicates that the alert is a warning.
+	SeverityWarning
+	// SeverityError indicates that the alert is an error.
+	SeverityError
+	// SeverityCritical indicates that the alert is critical.
+	SeverityCritical
+
+	severityInfoStr     = "info"
+	severityWarningStr  = "warning"
+	severityErrorStr    = "error"
+	severityCriticalStr = "critical"
+)
+
+type (
+	Severity uint8
+
+	// An Alert is a dismissible message that is displayed to the user.
+	Alert struct {
+		// ID is a unique identifier for the alert.
+		ID types.Hash256 `json:"id"`
+		// Severity is the severity of the alert.
+		Severity Severity `json:"severity"`
+		// Message is a human-readable message describing the alert.
+		Message string `json:"message"`
+		// Data is a map of arbitrary data that can be used to provide
+		// additional context to the alert.
+		Data map[string]any `json:"data,omitempty"`
+	}
+
+	// A Manager manages the host's alerts.
+	Manager struct {
+		mu sync.Mutex
+		// alerts is a map of alert IDs to their current alert.
+		alerts map[types.Hash256]Alert
+	}
+)
+
+// String implements the fmt.Stringer interface.
+func (s Severity) String() string {
+	switch s {
+	case SeverityInfo:
+		return severityInfoStr
+	case SeverityWarning:
+		return severityWarningStr
+	case SeverityError:
+		return severityErrorStr
+	case SeverityCritical:
+		return severityCriticalStr
+	default:
+		panic(fmt.Sprintf("unrecognized severity %d", s))
+	}
+}
+
+// MarshalJSON implements the json.Marshaler interface.
+func (s Severity) MarshalJSON() ([]byte, error) {
+	return []byte(fmt.Sprintf(`%q`, s.String())), nil
+}
+
+// UnmarshalJSON implements the json.Unmarshaler interface.
+func (s *Severity) UnmarshalJSON(b []byte) error {
+	status := strings.Trim(string(b), `"`)
+	switch status {
+	case severityInfoStr:
+		*s = SeverityInfo
+	case severityWarningStr:
+		*s = SeverityWarning
+	case severityErrorStr:
+		*s = SeverityError
+	case severityCriticalStr:
+		*s = SeverityCritical
+	default:
+		return fmt.Errorf("unrecognized severity: %v", status)
+	}
+	return nil
+}
+
+// Register registers a new alert with the manager
+func (m *Manager) Register(a Alert) {
+	if a.ID == (types.Hash256{}) {
+		panic("cannot register alert with empty ID") // developer error
+	}
+
+	m.mu.Lock()
+	m.alerts[a.ID] = a
+	m.mu.Unlock()
+}
+
+// Dismiss removes the alerts with the given IDs.
+func (m *Manager) Dismiss(ids ...types.Hash256) {
+	m.mu.Lock()
+	for _, id := range ids {
+		delete(m.alerts, id)
+	}
+	m.mu.Unlock()
+}
+
+// Alerts returns the host's current alerts.
+func (m *Manager) Alerts() []Alert {
+	m.mu.Lock()
+	defer m.mu.Unlock()
+
+	alerts := make([]Alert, 0, len(m.alerts))
+	for _, a := range m.alerts {
+		alerts = append(alerts, a)
+	}
+	return alerts
+}
+
+// NewManager initializes a new alerts manager.
+func NewManager() *Manager {
+	return &Manager{
+		alerts: make(map[types.Hash256]Alert),
+	}
+}
