f836798edcdef71114a100494ca4a0ee7e3cc017
Nate Maninger <me@n8m.us>
2023-08-25 09:29:00 -0600

    config: fix lint errors

--- config/config.go
+++ config/config.go
@@ -1,21 +1,25 @@
 package config
 
 type (
+	// HTTP contains the configuration for the HTTP server.
 	HTTP struct {
 		Address  string `yaml:"address"`
 		Password string `yaml:"password"`
 	}
 
+	// Consensus contains the configuration for the consensus set.
 	Consensus struct {
 		GatewayAddress string   `yaml:"gatewayAddress"`
 		Bootstrap      bool     `yaml:"bootstrap"`
 		Peers          []string `toml:"peers,omitempty"`
 	}
 
+	// RHP2 contains the configuration for the RHP2 server.
 	RHP2 struct {
 		Address string `yaml:"address"`
 	}
 
+	// RHP3 contains the configuration for the RHP3 server.
 	RHP3 struct {
 		TCPAddress       string `yaml:"tcp"`
 		WebSocketAddress string `yaml:"websocket"`
@@ -23,11 +27,13 @@ type (
 		KeyPath          string `yaml:"keyPath"`
 	}
 
+	// Log contains the configuration for the logger.
 	Log struct {
 		Path  string `yaml:"path"`
 		Level string `yaml:"level"`
 	}
 
+	// Config contains the configuration for the host.
 	Config struct {
 		Name           string `yaml:"name"`
 		Directory      string `yaml:"directory"`
