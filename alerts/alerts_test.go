aa16076a60868e58b92d0ac8fc73c023cba4b5c6
Nate <me@n8m.us>
2025-05-10 12:06:21 -0700

    fix test in CI

--- alerts/alerts_test.go
+++ alerts/alerts_test.go
@@ -1,7 +1,8 @@
 package alerts
 
 import (
-	"reflect"
+	"bytes"
+	"encoding/json"
 	"testing"
 	"time"
 
@@ -15,45 +16,49 @@ func TestAlerts(t *testing.T) {
 		ID:        frand.Entropy256(),
 		Severity:  SeverityCritical,
 		Message:   "foo",
-		Timestamp: time.Now().Truncate(time.Millisecond), // json rounds to milliseconds
+		Timestamp: time.Now(),
 	}
+
+	assertAlert := func() {
+		t.Helper()
+
+		alerts := m.Active()
+		if len(alerts) != 1 {
+			t.Fatalf("expected 1 alert, got %d", len(alerts))
+		}
+		ab, err := json.Marshal(alerts[0])
+		if err != nil {
+			t.Fatalf("failed to marshal alert: %v", err)
+		}
+		bb, err := json.Marshal(expectedAlert)
+		if err != nil {
+			t.Fatalf("failed to marshal expected alert: %v", err)
+		} else if !bytes.Equal(ab, bb) {
+			t.Fatalf("expected alert %s, got %s", string(bb), string(ab))
+		}
+	}
+
 	// register the alert
 	m.Register(expectedAlert)
-	alerts := m.Active()
-	if len(alerts) != 1 {
-		t.Fatalf("expected 1 alert, got %d", len(alerts))
-	} else if !reflect.DeepEqual(alerts[0], expectedAlert) {
-		t.Fatalf("expected alert %v, got %v", expectedAlert, alerts[0])
-	}
+	assertAlert()
 
 	// update the alert
 	expectedAlert.Data = map[string]any{
 		"bar": "baz",
 	}
 	m.Register(expectedAlert)
-	alerts = m.Active()
-	if len(alerts) != 1 {
-		t.Fatalf("expected 1 alert, got %d", len(alerts))
-	} else if !reflect.DeepEqual(alerts[0], expectedAlert) {
-		t.Fatalf("expected alert %v, got %v", expectedAlert, alerts[0])
-	}
+	assertAlert()
 
 	// update the alert
 	expectedAlert.Data = map[string]any{
 		"baz": "qux",
 	}
 	m.Register(expectedAlert)
-	alerts = m.Active()
-	if len(alerts) != 1 {
-		t.Fatalf("expected 1 alert, got %d", len(alerts))
-	} else if !reflect.DeepEqual(alerts[0], expectedAlert) {
-		t.Fatalf("expected alert %v, got %v", expectedAlert, alerts[0])
-	}
+	assertAlert()
 
 	// dismiss the alert
 	m.Dismiss(expectedAlert.ID)
-	alerts = m.Active()
-	if len(alerts) != 0 {
+	if alerts := m.Active(); len(alerts) != 0 {
 		t.Fatalf("expected 0 alerts, got %d", len(alerts))
 	}
 }
