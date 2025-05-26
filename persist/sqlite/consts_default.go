afa0e5d590bac63b4750e381a1f7fd116be8c38e
Nate <me@n8m.us>
2025-05-26 12:53:31 -0700

    remove unused consts

--- persist/sqlite/consts_default.go
+++ persist/sqlite/consts_default.go
@@ -2,14 +2,7 @@
 
 package sqlite
 
-import "time"
-
 const (
-	busyTimeout      = 10000 // 10 seconds
-	maxRetryAttempts = 30    // 30 attempts
-	factor           = 1.8   // factor ^ retryAttempts = backoff time in milliseconds
-	maxBackoff       = 15 * time.Second
-
 	// the number of records to limit long-running sector queries to
 	sqlSectorBatchSize = 256 // 1 GiB
 )
