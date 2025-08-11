77eb985e14d1b50d04ae1ce4112667a18f42a30c
Nate <me@n8m.us>
2025-08-11 12:18:00 -0700

    fix db note being closed

--- persist/sqlite/migrations_test.go
+++ persist/sqlite/migrations_test.go
@@ -527,6 +527,7 @@ VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $
 	if err != nil {
 		t.Fatal(err)
 	}
+	defer store.Close()
 
 	contract, err := store.V2Contract(contractID)
 	if err != nil {
